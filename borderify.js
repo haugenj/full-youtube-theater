5143d2722617bea770a3e9fc89535865572f93f2
Jason Haugen <jqhaugen2014@gmail.com>
2024-09-13 11:02:43 -0500

    notes for next change

--- borderify.js
+++ borderify.js
@@ -26,3 +26,14 @@ observer.observe(document.body, observerOptions)
 console.log("yoooooo")
 
 //style="border: 5px solid red;"
+
+//<div id="full-bleed-container" class="style-scope ytd-watch-flexy">
+    // player container
+
+// player controls class="ytp-chrome-controls"
+
+// might be able to solve this by changing the maxheight of the player controller to 100% - the size of the player controls
+// currently it's set to 100vh - 169px ?
+//  calc(100vh - 48px)
+
+// id = full-bleed-container
