a70f9cec1a8e001f50fc1bf49441764573d7ebdc
Jason Haugen <jqhaugen2014@gmail.com>
2024-09-19 11:16:35 -0500

    remove maxheight limit for fullscreen

--- theaterHeight.js
+++ theaterHeight.js
@@ -1,22 +1,31 @@
 const maxHeight = "calc(100vh - 58px)"
 const divId = "full-bleed-container"
 
-function changeMaxHeight(id) {
+function setMaxHeight(id) {
     const elem = document.getElementById(id)
     elem.style.maxHeight = maxHeight
+}
+
+function removeMaxHeight(id) {
+    const elem = document.getElementById(id)
+    elem.style.maxHeight = null
 }
 
 function observerCallback(mutationList, observer) {
     if (document.getElementById(divId)) {
         observer.disconnect();
-        changeMaxHeight(divId);
+        setMaxHeight(divId);
     }
 }
 
-const observerOptions = {
-    childList: true,
-    subtree: true,
-};
-
 const observer = new MutationObserver(observerCallback)
-observer.observe(document.body, observerOptions)
+observer.observe(document.body, {childList: true, subtree: true})
+
+// watch for fullscreen mode and then drop the maxHeight requirement - not supported on Safari
+document.addEventListener("fullscreenchange", () => { 
+    if (document.fullscreenElement) {
+        removeMaxHeight(divId)
+    } else {
+        setMaxHeight(divId)
+    }
+})
