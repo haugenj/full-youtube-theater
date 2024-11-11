db1cf1251503264d1d88861c5a13ed12d6f0f448
Jason Haugen <jqhaugen2014@gmail.com>
2024-11-11 12:18:32 -0600

    fix masthead visibility for fullscreen, default, and miniplayer
    use styles from otheraklsjdf

--- theaterHeight.js
+++ theaterHeight.js
@@ -1,41 +1,113 @@
 // Masthead height is 56px
 const maxHeightPaused = "calc(100vh - 56px)"
 const maxHeightPlaying = "100vh"
-const playerContainerId = "full-bleed-container"
+const playerContainerId = "player-full-bleed-container"
 const mastheadId = "masthead"
 const managerId = "page-manager"
+const movieId = "movie_player"
 
-function observerCallback(mutationList, observer) {
+let fullscreen = false
+let theater = false
+let miniplayer = false
+
+const states = {
+    pause: "pause",
+    play: "play"
+}
+let playerState = states.pause
+
+// ======
+// main
+// ======
+const observer = new MutationObserver(observerCallback)
+observer.observe(document.body, { childList: true, subtree: true })
+
+// ===========
+// callbacks
+// ===========
+function observerCallback(_mutationList, observer) {
     // wait for player to load first
     if (document.getElementById(playerContainerId)) {
+        //
+        // set initial player state
         const videoEl = document.getElementsByTagName('video')[0]
-        setMaxHeight(playerContainerId, maxHeightPaused);
+        if (document.getElementById(movieId).classList.contains("paused-mode")) {
+            playerState = states.pause
+        } else {
+            playerState = states.play
+            setMaxHeight(playerContainerId, maxHeightPlaying)
+        }
 
+        const ytdApp = document.getElementsByTagName("ytd-app")[0]
+        miniplayer = ytdApp.attributes.getNamedItem("miniplayer-is-active") !== null
+
+        const ytdWatchFlexy = document.getElementsByTagName("ytd-watch-flexy")[0]
+        theater = ytdWatchFlexy.attributes.getNamedItem("theater") !== null
+        
+        //
+        // event listeners
         videoEl.addEventListener('play', () => {
-            hideMastHead()
-            setMaxHeight(playerContainerId, maxHeightPlaying);
+            playerState = states.play
+            update()
         })
         videoEl.addEventListener('pause', () => {
-            showMastHead()
-            setMaxHeight(playerContainerId, maxHeightPaused)
+            playerState = states.pause
+            update()
         })
+
+        document.addEventListener("fullscreenchange", () => { // (fyi - not supported on Safari)
+            if (document.fullscreenElement) {
+                fullscreen = true
+                update()
+            } else {
+                fullscreen = false
+                update()
+            }
+        })
+
+        const miniPlayerObserver = new MutationObserver(miniPlayerCallback)
+        miniPlayerObserver.observe(ytdApp, { attributes: true })
+
+        const theaterObserver = new MutationObserver(theaterCallback)
+        theaterObserver.observe(ytdWatchFlexy, { attributes: true })
 
         observer.disconnect();
     }
 }
 
-const observer = new MutationObserver(observerCallback)
-observer.observe(document.body, { childList: true, subtree: true })
+function miniPlayerCallback(mutationList) {
+    miniplayer = mutationList[0].target.attributes.getNamedItem("miniplayer-is-active") !== null
+    update()
+}
 
-// watch for fullscreen mode and then drop the maxHeight requirement - not supported on Safari
-document.addEventListener("fullscreenchange", () => {
-    if (document.fullscreenElement) {
+function theaterCallback(mutationList) {
+    theater = mutationList[0].target.attributes.getNamedItem("theater") !== null
+    update()
+}
+
+// =======
+// update
+// =======
+function update() {
+    if (fullscreen) {
         removeMaxHeight(playerContainerId)
+        hideMastHead()
+    } else if (!miniplayer && theater) {
+        if (playerState == states.play) {
+            hideMastHead()
+            setMaxHeight(playerContainerId, maxHeightPlaying)
+        } else {
+            showMastHead()
+            setMaxHeight(playerContainerId, maxHeightPaused)
+        }
     } else {
-        setMaxHeight(playerContainerId)
+        showMastHead()
     }
-})
+}
 
+// ========
+// helpers
+// ========
 function setMaxHeight(id, height) {
     const elem = document.getElementById(id)
     elem.style.maxHeight = height
